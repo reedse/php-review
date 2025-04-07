34719731c1a6c9acfae0a68f263ed4efde4ec320
reedse_7033251 <111336292+reedse@users.noreply.github.com>
2025-04-07 12:35:17 -0400

    updated fibonacci.php

--- data-structures/fibonacci.php
+++ data-structures/fibonacci.php
@@ -26,6 +26,18 @@
 
 function fibonacci($n) {
     // Your code here
+    //recursive method
+
+    //base cases:
+    if ($n == 0) {
+        return 0;
+    }
+
+    if ($n === 1) {
+        return 1;
+    }
+
+    return fibonacci($n-1) + fibonacci($n-2);
 }
 
 // Test cases:
